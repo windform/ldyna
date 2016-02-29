290543d69bb96bd243847549a67b86298ce9d70b
liaodong <867185661@qq.com>
2016-02-29 13:32:27 +0800

    jQuery模板引擎技术源码

---
+++ jQuery_templ/js/template.js
@@ -0,0 +1,9 @@
+
+	<script id="comics" type="text/x-jquery-tmpl">
+		<div class="col-md-3">
+			<div class="img">${imgSrc}</div>
+			<h3>${title}</h3>
+			<p class="year">Year:${year}</p>
+			<p class="number">Issue Number:${number}</p>
+		</div>
+	</script>
